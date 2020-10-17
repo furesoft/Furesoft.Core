bcde4ad6720fd8a1ece4eb7691a97887e79b1ecb
Chris Anders <filmee24@gmail.com>
2020-10-17 13:23:29 +0200

    Cleanup

--- Furesoft.Core/Signals/Optional.cs
+++ Furesoft.Core/Signals/Optional.cs
@@ -1,59 +1,59 @@
 ﻿namespace Furesoft.Core.Signals
 {
-    public static class ObjectExtentions
-    {
-        public static Optional<T> ToOptional<T>(this T target)
-        {
-            return Optional<T>.Some(target);
-        }
-    }
+	public static class ObjectExtentions
+	{
+		public static Optional<T> ToOptional<T>(this T target)
+		{
+			return Optional<T>.Some(target);
+		}
+	}
 
-    public class Optional<T>
-    {
-        public static Optional<T> None => new Optional<T>();
+	public class Optional<T>
+	{
+		public static Optional<T> None => new Optional<T>();
 
-        public bool HasValue { get; }
+		public bool HasValue { get; }
 
-        public T Value { get; }
+		public T Value { get; }
 
-        public Optional(T value)
-        {
-            if (value != null)
-            {
-                HasValue = true;
-                Value = value;
-            }
-        }
+		public Optional(T value)
+		{
+			if (value != null)
+			{
+				HasValue = true;
+				Value = value;
+			}
+		}
 
-        public Optional()
-        {
-        }
+		public Optional()
+		{
+		}
 
-        public static implicit operator bool(Optional<T> opt)
-        {
-            return opt.HasValue;
-        }
+		public static implicit operator bool(Optional<T> opt)
+		{
+			return opt.HasValue;
+		}
 
-        public static implicit operator Optional<T>(bool value) => Optional<T>.None;
+		public static implicit operator Optional<T>(bool value) => None;
 
-        public static implicit operator T(Optional<T> opt)
-        {
-            if (opt.HasValue)
-            {
-                return opt.Value;
-            }
+		public static implicit operator T(Optional<T> opt)
+		{
+			if (opt.HasValue)
+			{
+				return opt.Value;
+			}
 
-            return default(T);
-        }
+			return default;
+		}
 
-        public static Optional<TValue> Some<TValue>(TValue value)
-        {
-            return new Optional<TValue>(value);
-        }
+		public static Optional<TValue> Some<TValue>(TValue value)
+		{
+			return new Optional<TValue>(value);
+		}
 
-        public override string ToString()
-        {
-            return Value.ToString();
-        }
-    }
+		public override string ToString()
+		{
+			return Value.ToString();
+		}
+	}
 }
--- Furesoft.Core/Signals/Signal.cs
+++ Furesoft.Core/Signals/Signal.cs
@@ -10,254 +10,248 @@ using System.Threading.Tasks;
 
 namespace Furesoft.Core.Signals
 {
-    public static class Signal
-    {
-        public static IpcChannel OpenChannel(string name)
-        {
-            try
-            {
-                MemoryMappedFile.OpenExisting(name);
-                return CreateRecieverChannel(name);
-            }
-            catch (FileNotFoundException ex)
-            {
-                return CreateSenderChannel(name);
-            }
-        }
+	public static class Signal
+	{
+		public static IpcChannel OpenChannel(string name)
+		{
+			try
+			{
+				MemoryMappedFile.OpenExisting(name);
+				return CreateRecieverChannel(name);
+			}
+			catch (FileNotFoundException)
+			{
+				return CreateSenderChannel(name);
+			}
+		}
 
-        public static ISerializer Serializer = new JsonSerializer();
+		public static ISerializer Serializer = new JsonSerializer();
 
-        public static void CallEvent<EventType>(IpcChannel channel, EventType et)
-        {
-            var objid = typeof(EventType).GUID;
-            var serialized = Serializer.Serialize(et);
+		public static void CallEvent<EventType>(IpcChannel channel, EventType et)
+		{
+			var objid = typeof(EventType).GUID;
+			var serialized = Serializer.Serialize(et);
 
-            var ms = new MemoryStream();
-            var bw = new BinaryWriter(ms);
+			var ms = new MemoryStream();
+			var bw = new BinaryWriter(ms);
 
-            bw.Write(objid.ToByteArray());
-            bw.Write(serialized);
+			bw.Write(objid.ToByteArray());
+			bw.Write(serialized);
 
-            var raw = ms.ToArray();
+			var raw = ms.ToArray();
 
-            channel.event_communicator.Write(raw);
-        }
+			channel.event_communicator.Write(raw);
+		}
 
-        public static T CallMethod<T>(IpcChannel channel, int id, params object[] arg)
-        {
-            return Task.Run(() => CallMethodAsync<T>(channel, id, arg)).Result;
-        }
+		public static T CallMethod<T>(IpcChannel channel, int id, params object[] arg)
+		{
+			return Task.Run(() => CallMethodAsync<T>(channel, id, arg)).Result;
+		}
 
-        public static Task<T> CallMethodAsync<T>(IpcChannel channel, int id, params object[] arg)
-        {
-            TaskCompletionSource<T> tcs = new TaskCompletionSource<T>();
+		public static Task<T> CallMethodAsync<T>(IpcChannel channel, int id, params object[] arg)
+		{
+			var tcs = new TaskCompletionSource<T>();
 
-            channel.func_communicator.OnNewMessage += (data) =>
-            {
-                var resp = Serializer.Deserialize<FunctionCallResponse>(data);
+			channel.func_communicator.OnNewMessage += (data) =>
+			{
+				var resp = Serializer.Deserialize<FunctionCallResponse>(data);
 
-                if (resp.ID == id)
-                {
-                    if (string.IsNullOrEmpty(resp.ErrorMessage))
-                    {
-                        if (resp.ReturnValue != null)
-                        {
-                            if (typeof(T) == typeof(JObject))
-                            {
-                                tcs.TrySetResult((T)JsonConvert.DeserializeObject(Serializer.Deserialize<string>(resp.ReturnValue)));
-                            }
-                            else
-                            {
-                                tcs.TrySetResult(Serializer.Deserialize<T>(resp.ReturnValue));
-                            }
-                        }
-                    }
-                    else
-                    {
-                        var ex = new Exception(resp.ErrorMessage);
+				if (resp.ID == id)
+				{
+					if (string.IsNullOrEmpty(resp.ErrorMessage))
+					{
+						if (resp.ReturnValue != null)
+						{
+							if (typeof(T) == typeof(JObject))
+							{
+								tcs.TrySetResult((T)JsonConvert.DeserializeObject(Serializer.Deserialize<string>(resp.ReturnValue)));
+							}
+							else
+							{
+								tcs.TrySetResult(Serializer.Deserialize<T>(resp.ReturnValue));
+							}
+						}
+					}
+					else
+					{
+						var ex = new Exception(resp.ErrorMessage);
 
-                        tcs.TrySetException(ex);
-                    }
-                }
-            };
+						tcs.TrySetException(ex);
+					}
+				}
+			};
 
-            var m = new FunctionCallRequest
-            {
-                ID = id,
-                ParameterRaw = arg.Select(_ => Serializer.Serialize(_)).ToArray()
-            };
+			var m = new FunctionCallRequest
+			{
+				ID = id,
+				ParameterRaw = arg.Select(_ => Serializer.Serialize(_)).ToArray()
+			};
 
-            var raw = Serializer.Serialize(m);
+			var raw = Serializer.Serialize(m);
 
-            channel.func_communicator.Write(raw);
+			channel.func_communicator.Write(raw);
 
-            return tcs.Task;
-        }
+			return tcs.Task;
+		}
 
-        public static void CollectAllShared(IpcChannel channel)
-        {
-            var assembly = Assembly.GetCallingAssembly();
+		public static void CollectAllShared(IpcChannel channel)
+		{
+			var assembly = Assembly.GetCallingAssembly();
 
-            CollectShared(channel, assembly.GetTypes());
-        }
+			CollectShared(channel, assembly.GetTypes());
+		}
 
-        public static void CollectShared(IpcChannel channel, params Type[] types)
-        {
-            channel.func_communicator.OnNewMessage += (data) =>
-            {
-                var obj = Serializer.Deserialize<FunctionCallRequest>(data);
+		public static void CollectShared(IpcChannel channel, params Type[] types)
+		{
+			channel.func_communicator.OnNewMessage += (data) =>
+			{
+				var obj = Serializer.Deserialize<FunctionCallRequest>(data);
 
-                Optional<string> error = false;
-                object res = null;
-                object[] args = null;
+				Optional<string> error = false;
+				object res = null;
+				object[] args = null;
 
-                var methodInfo = channel.shared_functions[obj.ID];
-                var parameterInfo = methodInfo.GetParameters();
+				var methodInfo = channel.shared_functions[obj.ID];
+				var parameterInfo = methodInfo.GetParameters();
 
-                if (!IsArgumentMismatch(parameterInfo, obj.ParameterRaw))
-                {
-                    args = GetDeserializedParameters(parameterInfo, obj.ParameterRaw);
-                }
+				if (!IsArgumentMismatch(parameterInfo, obj.ParameterRaw))
+				{
+					args = GetDeserializedParameters(parameterInfo, obj.ParameterRaw);
+				}
 
-                var id = methodInfo.GetCustomAttribute<SharedFunctionAttribute>()?.ID;
+				var id = methodInfo.GetCustomAttribute<SharedFunctionAttribute>()?.ID;
 
-                try
-                {
-                    try
-                    {
-                        if (methodInfo.IsStatic)
-                        {
-                            res = methodInfo.Invoke(null, args);
-                        }
-                        else
-                        {
-                            res = methodInfo.Invoke(channel, args);
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        error = ex.Message.ToOptional();
-                    }
-                }
-                catch (Exception ex)
-                {
-                    error = ex.Message.ToOptional();
-                }
+				try
+				{
+					try
+					{
+						if (methodInfo.IsStatic)
+						{
+							res = methodInfo.Invoke(null, args);
+						}
+						else
+						{
+							res = methodInfo.Invoke(channel, args);
+						}
+					}
+					catch (Exception ex)
+					{
+						error = ex.Message.ToOptional();
+					}
+				}
+				catch (Exception ex)
+				{
+					error = ex.Message.ToOptional();
+				}
 
-                var resp = new FunctionCallResponse
-                {
-                    ID = obj.ID
-                };
+				var resp = new FunctionCallResponse
+				{
+					ID = obj.ID
+				};
 
-                if (!error)
-                {
-                    resp.ReturnValue = Serializer.Serialize(res);
-                }
-                else
-                {
-                    resp.ErrorMessage = error;
-                }
+				if (!error)
+				{
+					resp.ReturnValue = Serializer.Serialize(res);
+				}
+				else
+				{
+					resp.ErrorMessage = error;
+				}
 
-                var raw = Serializer.Serialize(resp);
-                channel.func_communicator.Write(raw);
-            };
+				var raw = Serializer.Serialize(resp);
+				channel.func_communicator.Write(raw);
+			};
 
-            foreach (var t in types)
-            {
-                var attr = t.GetCustomAttribute<SharedAttribute>();
+			foreach (var t in types)
+			{
+				var attr = t.GetCustomAttribute<SharedAttribute>();
 
-                if (attr != null)
-                {
-                    foreach (var m in t.GetMethods())
-                    {
-                        var mattr = m.GetCustomAttribute<SharedFunctionAttribute>();
+				if (attr != null)
+				{
+					foreach (var m in t.GetMethods())
+					{
+						var mattr = m.GetCustomAttribute<SharedFunctionAttribute>();
 
-                        if (mattr != null)
-                        {
-                            if (!channel.shared_functions.ContainsKey(mattr.ID))
-                            {
-                                channel.shared_functions.Add(mattr.ID, m);
-                            }
-                        }
-                    }
-                }
-            }
-        }
+						if (mattr != null)
+						{
+							if (!channel.shared_functions.ContainsKey(mattr.ID))
+							{
+								channel.shared_functions.Add(mattr.ID, m);
+							}
+						}
+					}
+				}
+			}
+		}
 
-        public static IpcChannel CreateRecieverChannel<TBackend>(string name)
-            where TBackend : ISignalBackend, new()
-        {
-            var channel = new IpcChannel
-            {
-                event_communicator = new TBackend(),
-                func_communicator = new TBackend()
-            };
+		public static IpcChannel CreateRecieverChannel<TBackend>(string name)
+			where TBackend : ISignalBackend, new()
+		{
+			var channel = new IpcChannel
+			{
+				event_communicator = new TBackend(),
+				func_communicator = new TBackend()
+			};
 
-            channel.event_communicator.Initialize(name + ".events", 4096, true);
-            channel.func_communicator.Initialize(name + ".funcs", 4096, true);
+			channel.event_communicator.Initialize(name + ".events", 4096, true);
+			channel.func_communicator.Initialize(name + ".funcs", 4096, true);
 
-            return channel;
-        }
+			return channel;
+		}
 
-        private static IpcChannel CreateRecieverChannel<TBackend>(int name)
-            where TBackend : ISignalBackend, new()
-        {
-            return CreateRecieverChannel<TBackend>(name.ToString());
-        }
+		private static IpcChannel CreateRecieverChannel(string name)
+		{
+			return CreateRecieverChannel<MmfCommunicatorBackend>(name);
+		}
 
-        private static IpcChannel CreateRecieverChannel(string name)
-        {
-            return CreateRecieverChannel<MmfCommunicatorBackend>(name);
-        }
+		private static IpcChannel CreateSenderChannel(string name)
+		{
+			return CreateSenderChannel<MmfCommunicatorBackend>(name);
+		}
 
-        private static IpcChannel CreateSenderChannel(string name)
-        {
-            return CreateSenderChannel<MmfCommunicatorBackend>(name);
-        }
+		private static IpcChannel CreateSenderChannel<TBackend>(string name)
+			where TBackend : ISignalBackend, new()
+		{
+			var channel = new IpcChannel
+			{
+				event_communicator = new TBackend(),
+				func_communicator = new TBackend()
+			};
 
-        private static IpcChannel CreateSenderChannel<TBackend>(string name)
-            where TBackend : ISignalBackend, new()
-        {
-            var channel = new IpcChannel
-            {
-                event_communicator = new TBackend(),
-                func_communicator = new TBackend()
-            };
+			channel.event_communicator.Initialize(name + ".events", 4096, false);
+			channel.func_communicator.Initialize(name + ".funcs", 4096, false);
 
-            channel.event_communicator.Initialize(name + ".events", 4096, false);
-            channel.func_communicator.Initialize(name + ".funcs", 4096, false);
+			return channel;
+		}
 
-            return channel;
-        }
+		public static void Subscribe<EventType>(IpcChannel channel, Action<EventType> callback)
+		{
+			channel.event_communicator.OnNewMessage += (data) =>
+			{
+				var objid = typeof(EventType).GUID;
 
-        public static void Subscribe<EventType>(IpcChannel channel, Action<EventType> callback)
-        {
-            channel.event_communicator.OnNewMessage += (data) =>
-            {
-                var objid = typeof(EventType).GUID;
+				if (objid == new Guid(data.Take(16).ToArray()))
+				{
+					var obj = Serializer.Deserialize<EventType>(data.Skip(16).ToArray());
 
-                if (objid == new Guid(data.Take(16).ToArray()))
-                {
-                    var obj = Serializer.Deserialize<EventType>(data.Skip(16).ToArray());
+					callback(obj);
+				}
+			};
+		}
 
-                    callback(obj);
-                }
-            };
-        }
+		private static object[] GetDeserializedParameters(ParameterInfo[] parameterInfo, byte[][] parameterRaw)
+		{
+			var res = new List<object>();
+			for (var i = 0; i < parameterRaw.Length; i++)
+			{
+				res.Add(Serializer.Deserialize(parameterRaw[i], parameterInfo[i].ParameterType));
+			}
+			return res.ToArray();
+		}
 
-        private static object[] GetDeserializedParameters(ParameterInfo[] parameterInfo, byte[][] parameterRaw)
-        {
-            var res = new List<object>();
-            for (int i = 0; i < parameterRaw.Length; i++)
-            {
-                res.Add(Serializer.Deserialize(parameterRaw[i], parameterInfo[i].ParameterType));
-            }
-            return res.ToArray();
-        }
-
-        private static bool IsArgumentMismatch(ParameterInfo[] parameterInfo, byte[][] parameterJson)
-        {
-            return parameterInfo.Length != parameterJson.Length;
-        }
-    }
+		private static bool IsArgumentMismatch(ParameterInfo[] parameterInfo, byte[][] parameterJson)
+		{
+			return parameterInfo.Length != parameterJson.Length;
+		}
+	}
 }
