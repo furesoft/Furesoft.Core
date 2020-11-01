7659f6b804096dd3936297884f67f543c7d37964
Chris Anders <filmee24@gmail.com>
2020-11-01 17:35:40 +0100

    Add CommandParser

---
+++ Furesoft.Core/Commands/Command.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Furesoft.Core.Commands
+{
+	public class Command
+	{
+		public string Name { get; set; }
+		public List<object> Args { get; set; } = new List<object>();
+
+		public T GetArg<T>(int index)
+		{
+			if (Args.Count == 0) return default;
+			return (T)Args[index];
+		}
+	}
+}
