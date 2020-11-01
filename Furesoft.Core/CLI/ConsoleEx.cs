d718039e89ec59c16e0df19517305691a083bd8a
Chris Anders <filmee24@gmail.com>
2020-11-01 17:05:21 +0100

    Add ConsoleEx Class

---
+++ Furesoft.Core/CLI/ConsoleEx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Furesoft.Core.CLI
+{
+	public static class ConsoleEx
+	{
+		public static string Prompt(string message)
+		{
+			Console.Write(message);
+			return Console.ReadLine();
+		}
+
+		public static int PromptInt(string message)
+		{
+			var value = Prompt(message);
+			if (int.TryParse(value, out var res))
+			{
+				return res;
+			}
+
+			Console.WriteLine("Invalid Value");
+
+			return -1;
+		}
+
+		public static float PromptDecimal(string message)
+		{
+			var value = Prompt(message);
+			if (float.TryParse(value, out var res))
+			{
+				return res;
+			}
+
+			Console.WriteLine("Invalid Value");
+
+			return -1;
+		}
+	}
+}
