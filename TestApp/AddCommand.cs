cfe58316dbb1699dfb687dadf1ea274a2e3e3113
Chris Anders <filmee24@gmail.com>
2020-11-01 18:49:25 +0100

    Cleanup

---
+++ TestApp/AddCommand.cs
@@ -0,0 +1,25 @@
+﻿using Furesoft.Core.AST;
+using Furesoft.Core.CLI;
+using Furesoft.Core.Commands;
+using System;
+using System.IO;
+
+namespace TestApp
+{
+    public class AddCommand : ICommand
+    {
+        public string Name => "add";
+
+        public string HelpText => "add <id> (--optimize|-opt)?";
+
+        public string Description => "Add Article from Site to Book";
+
+        public int Invoke(ArgumentVector args)
+        {
+            var id = args.GetValue<int>(0);
+            var optimize = args.GetOption("-opt|--optimize");
+
+            return 0;
+        }
+    }
+}
