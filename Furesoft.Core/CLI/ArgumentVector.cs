e800127c969232b6a5caf8847f7428379eea7491
Chris Anders <filmee24@gmail.com>
2020-11-01 16:39:14 +0100

    ABI Improvement Of ArgumentVector

--- Furesoft.Core/CLI/ArgumentVector.cs
+++ Furesoft.Core/CLI/ArgumentVector.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -39,6 +40,11 @@ namespace Furesoft.Core.CLI
 			}
 
 			return false;
+		}
+
+		public bool GetOption(string shortName, string longName)
+		{
+			return GetOption($"-{shortName}|--{longName}");
 		}
 	}
 }
