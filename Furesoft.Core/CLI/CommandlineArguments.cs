08dd61a6d87eed4aae71ac2f5cd922d6058f714a
Chris Anders <filmee24@gmail.com>
2020-11-08 09:51:37 +0100

    Cleanup

--- Furesoft.Core/CLI/CommandlineArguments.cs
+++ Furesoft.Core/CLI/CommandlineArguments.cs
@@ -85,15 +85,12 @@ namespace Furesoft.Core.CLI
 			get { return Parameters[param]; }
 		}
 
-		public string this[int index]
+		public string GetKey(int index)
 		{
-			get
-			{
-				var p = new string[Parameters.Count];
-				Parameters.Values.CopyTo(p, 0);
+			var p = new string[Parameters.Count];
+			Parameters.Keys.CopyTo(p, 0);
 
-				return p[index];
-			}
+			return p[index];
 		}
 
 		public T GetValue<T>(string param)
