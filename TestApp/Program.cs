b443775fb859b803b4734087a15d2da59c768de7
filmee24 <filmee24@gmail.com>
2021-09-26 19:25:08 +0200

    Cleanup

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -10,8 +10,16 @@ namespace TestApp
             ExpressionParser.AddVariable("x", 42);
             //ExpressionParser.Evaluate("g(x) = x*x");
 
-            var result = ExpressionParser.Evaluate("f: x in N  x > 2 < 20; f(x) = 2*x; f(1)");
+            var result = ExpressionParser.Evaluate("f: x in N  2 < x < 20; f(x) = 2*x; f(5)");
             //f: x is N {2,10};
+
+            //ToDo: implement constraint for interval
+            //ToDo: implement clr functions
+            //ToDo: implement exponent
+            //ToDo: if a function has 2 arguments but called with 1 attach error
+            //ToDo: add value expression: |-12|
+            //ToDo: implement tests
+            //
 
             return App.Current.Run();
         }
