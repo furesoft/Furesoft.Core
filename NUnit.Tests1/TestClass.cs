c6b6390b1b5bf1f6d7827c0c6b570ca1096adc3a
filmee24 <filmee24@gmail.com>
2021-10-01 19:43:08 +0200

    Add Tests

--- NUnit.Tests1/TestClass.cs
+++ NUnit.Tests1/TestClass.cs
@@ -1,7 +1,8 @@
 ﻿// NUnit 3 tests
 // See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using Furesoft.Core.ExpressionEvaluator;
 using NUnit.Framework;
-using System.Collections;
+using System;
 using System.Collections.Generic;
 
 namespace NUnit.Tests1
@@ -9,12 +10,50 @@ namespace NUnit.Tests1
     [TestFixture]
     public class TestClass
     {
-        [Test]
-        public void TestMethod()
+        static TestClass()
         {
-            // TODO: Add your test code here
-            var answer = 42;
-            Assert.That(answer, Is.EqualTo(42), "Some useful error message");
+            ExpressionParser.Init();
+        }
+
+        private static IEnumerable<TestCaseData> TestData
+        {
+            get
+            {
+                yield return new TestCaseData("log10(100)", Math.Log10(100));
+                yield return new TestCaseData("floor(PI)", Math.Floor(Math.PI));
+                yield return new TestCaseData("sin(2 * 4 + floor(PI))", Math.Sin(2 * 4 + Math.Floor(Math.PI)));
+                yield return new TestCaseData("areaTriangle(width, height) = width * height / 2; areaTriangle(5,10);", 25);
+                yield return new TestCaseData("use \"geometry.math\"; areaRectangle(5, 3);", 15);
+                yield return new TestCaseData("g: x in N [5, INFINITY];g(x) = x*x; g(6);", 36);
+                yield return new TestCaseData("|-4**2|;", 16);
+                yield return new TestCaseData("-|4**2|;", -16);
+                yield return new TestCaseData("use \"geometry.math\"; -areaRectangle(5, 3);", -15);
+            }
+        }
+
+        [Test, TestCaseSource(nameof(TestData))]
+        public void FunctionCall_Should_Pass(string input, double expected)
+        {
+            ExecuteTest(input, expected);
+        }
+
+        [TestCase("4*5", 20)]
+        [TestCase("3+4*5", 23)]
+        [TestCase("(3+4)*5", 35)]
+        [TestCase("PI*2", Math.PI * 2)]
+        public void SimpleExpressions_Should_Pass(string input, double expected)
+        {
+            ExecuteTest(input, expected);
+        }
+
+        private void ExecuteTest(string input, double expected)
+        {
+            var ep = new ExpressionParser();
+            ep.RootScope.Import(typeof(Math));
+
+            var result = ep.Evaluate(input);
+
+            Assert.That(result.Values, Does.Contain(expected), "Calculation is wrong");
         }
     }
 }
