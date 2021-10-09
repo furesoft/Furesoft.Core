400eb27c117ec1c2287b004ce748251ec1ae00ea
filmee24 <filmee24@gmail.com>
2021-10-09 08:40:20 +0200

    Add Macro Class

---
+++ Furesoft.Core.ExpressionEvaluator/Macro.cs
@@ -0,0 +1,17 @@
+﻿using Furesoft.Core.CodeDom.CodeDOM.Base;
+using Furesoft.Core.CodeDom.CodeDOM.Expressions.Base;
+using System.Collections.Generic;
+
+namespace Furesoft.Core.ExpressionEvaluator
+{
+    public delegate Expression MacroInvokeDelegate(MacroContext context, params Expression[] arguments);
+
+    public abstract class Macro
+    {
+        public List<ChildList<Expression>> Rules = new();
+
+        public abstract string Name { get; }
+
+        public abstract Expression Invoke(MacroContext context, params Expression[] arguments);
+    }
+}
