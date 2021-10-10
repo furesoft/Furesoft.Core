cb75d491d93b3f6595e2d5ee099922a1dc0a4934
filmee24 <filmee24@gmail.com>
2021-10-10 12:57:24 +0200

    Add Geometric Functions

--- Furesoft.Core.ExpressionEvaluator/Library/Geometry.cs
+++ Furesoft.Core.ExpressionEvaluator/Library/Geometry.cs
@@ -5,6 +5,30 @@ namespace Furesoft.Core.ExpressionEvaluator.Library
     [Module("geometry")]
     public static class Geometry
     {
+        [FunctionName("areaRectangle")]
+        public static double AreaRectangle(double width, double height)
+        {
+            return width * height;
+        }
+
+        [FunctionName("areaSquare")]
+        public static double AreaSquare(double width)
+        {
+            return width * width;
+        }
+
+        [FunctionName("areaSquare")]
+        public static double AreaSquare(double width, double height)
+        {
+            return width * height;
+        }
+
+        [FunctionName("areaTriangle")]
+        public static double AreaTriangle(double width, double height)
+        {
+            return width * height / 2;
+        }
+
         [FunctionName("circumference")]
         public static double Circumference(double radius)
         {
