10d3900989be826fde32bfd0f86fbbc717c8311b
filmee24 <filmee24@gmail.com>
2022-04-22 10:24:33 +0200

    Add DescribedAttribute

---
+++ Source/Furesoft.CodeDom/Furesoft.Core.CodeDom.Compiler/TypeSystem/DescribedAttribute.cs
@@ -0,0 +1,16 @@
+﻿using Furesoft.Core.CodeDom.Compiler.Core;
+
+namespace Furesoft.Core.CodeDom.Compiler.TypeSystem
+{
+    public class DescribedAttribute : IAttribute
+    {
+        private readonly IType _attributeType;
+
+        public DescribedAttribute(IType attributeType)
+        {
+            this._attributeType = attributeType;
+        }
+
+        public IType AttributeType => _attributeType;
+    }
+}
