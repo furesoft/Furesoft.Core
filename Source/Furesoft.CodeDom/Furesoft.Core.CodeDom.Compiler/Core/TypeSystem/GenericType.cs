2eb868118e1fc75553636ccefef1b2b8fbc46646
filmee24 <filmee24@gmail.com>
2022-08-11 19:16:31 +0200

    Add GenericType

---
+++ Source/Furesoft.CodeDom/Furesoft.Core.CodeDom.Compiler/Core/TypeSystem/GenericType.cs
@@ -0,0 +1,17 @@
+﻿using Furesoft.Core.CodeDom.Compiler.Core.Names;
+
+namespace Furesoft.Core.CodeDom.Compiler.Core.TypeSystem
+{
+    public class GenericType : DescribedType
+    {
+        public GenericType(QualifiedName fullName, IAssembly assembly) : base(fullName, assembly)
+        {
+        }
+
+        public GenericType(UnqualifiedName name, IType parentType) : base(name, parentType)
+        {
+        }
+
+        public List<IType> GenericArguments { get; set; } = new();
+    }
+}
