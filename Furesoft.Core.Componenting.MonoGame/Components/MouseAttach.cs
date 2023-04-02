01024a6824ff5aba09e8674a6bf58203bb5dc02e
Chris <filmee24@gmail.com>
2023-04-02 09:45:39 +0200

    feat: Add MouseAttach Component

---
+++ Furesoft.Core.Componenting.MonoGame/Components/MouseAttach.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace Furesoft.Core.Componenting.MonoGame.Components;
+
+public class MouseAttach : GameComponent
+{
+    public override void Update(GameTime gameTime)
+    {
+        var state = Mouse.GetState();
+
+        Object.GetComponent<TransformComponent>().Position = state.Position.ToVector2();
+    }
+}
