5de22f13933c24d8d51bd750dffad0fa5f90542d
furesoft <filmee24@gmail.com>
2023-05-16 09:52:35 +0200

    refactor: Cleanup

---
+++ Source/Furesoft.Core/Rules/DSL/Grammar.cs
@@ -0,0 +1,13 @@
+﻿using Furesoft.PrattParser;
+using Furesoft.PrattParser.Expressions;
+
+namespace Furesoft.Core.Rules.DSL;
+
+public class Grammar : Parser<IAstNode>
+{
+    public Grammar(ILexer lexer) : base(lexer)
+    {
+        Prefix("!", (int)BindingPower.Prefix);
+        Prefix("not", (int)BindingPower.Prefix);
+    }
+}
---
+++ Source/RulesTest/TestDsl.cs
@@ -0,0 +1,19 @@
+﻿using Furesoft.PrattParser;
+using Xunit;
+
+namespace RulesTest;
+
+public class TestDsl
+{
+    [Fact]
+    public void Not_Should_Pass()
+    {
+        var lexer = new Lexer("!5");
+        lexer.AddKeyword("not");
+        //lexer.AddSymbol("!");
+        
+        var parser = new Furesoft.Core.Rules.DSL.Grammar(lexer);
+
+        var node = parser.Parse();
+    }
+}
