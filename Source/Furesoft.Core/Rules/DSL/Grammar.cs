2d1d1c3d795f2228b3541ac0ad172c425004da59
Chris <filmee24@gmail.com>
2023-05-16 19:59:25 +0200

    refactor: Cleanup

--- Source/Furesoft.Core/Rules/DSL/Grammar.cs
+++ Source/Furesoft.Core/Rules/DSL/Grammar.cs
@@ -1,29 +1,14 @@
 ﻿using Furesoft.PrattParser;
 using Furesoft.PrattParser.Nodes;
-using Furesoft.PrattParser.Parselets;
 
 namespace Furesoft.Core.Rules.DSL;
 
 public class Grammar : Parser<AstNode>
 {
-    public Grammar(ILexer lexer) : base(lexer)
+    public Grammar(Lexer tokens) : base(tokens)
     {
-        Register(PredefinedSymbols.Integer, new IntegerLiteralParselet());
-        
         Group("(", ")");
-
-        Prefix("+", (int)BindingPower.Prefix);
+        
         Prefix("-", (int)BindingPower.Prefix);
-        Prefix("~", (int)BindingPower.Prefix);
-        Prefix("!", (int)BindingPower.Prefix);
-        Prefix("not", (int)BindingPower.Prefix);
-
-        Postfix("!", (int)BindingPower.PostFix);
-
-        InfixLeft("+", (int)BindingPower.Sum);
-        InfixLeft("-", (int)BindingPower.Sum);
-        InfixLeft("*", (int)BindingPower.Product);
-        InfixLeft("/", (int)BindingPower.Product);
-        InfixRight("^", (int)BindingPower.Exponent);
     }
 }
