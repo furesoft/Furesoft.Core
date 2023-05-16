eb64a62ee3814eb81c0cebc143e5ad2cb08318c9
furesoft <filmee24@gmail.com>
2023-05-16 12:15:48 +0200

    chore(deps): Update Packages

--- Source/Furesoft.Core/Rules/DSL/Grammar.cs
+++ Source/Furesoft.Core/Rules/DSL/Grammar.cs
@@ -1,5 +1,6 @@
 ﻿using Furesoft.PrattParser;
 using Furesoft.PrattParser.Expressions;
+using Furesoft.PrattParser.Parselets;
 
 namespace Furesoft.Core.Rules.DSL;
 
@@ -7,7 +8,21 @@ public class Grammar : Parser<IAstNode>
 {
     public Grammar(ILexer lexer) : base(lexer)
     {
+        Register(PredefinedSymbols.Integer, new IntegerLiteralParselet());
+        
+        Group("(", ")");
+
+        Prefix("+", (int)BindingPower.Prefix);
+        Prefix("-", (int)BindingPower.Prefix);
+        Prefix("~", (int)BindingPower.Prefix);
         Prefix("!", (int)BindingPower.Prefix);
-        Prefix("not", (int)BindingPower.Prefix);
+
+        Postfix("!", (int)BindingPower.PostFix);
+
+        InfixLeft("+", (int)BindingPower.Sum);
+        InfixLeft("-", (int)BindingPower.Sum);
+        InfixLeft("*", (int)BindingPower.Product);
+        InfixLeft("/", (int)BindingPower.Product);
+        InfixRight("^", (int)BindingPower.Exponent);
     }
 }
--- Source/RulesTest/TestDsl.cs
+++ Source/RulesTest/TestDsl.cs
@@ -9,7 +9,7 @@ public class TestDsl
     public void Not_Should_Pass()
     {
         var lexer = new Lexer("!5");
-        lexer.AddKeyword("not");
+        //lexer.AddKeyword("not");
         //lexer.AddSymbol("!");
         
         var parser = new Furesoft.Core.Rules.DSL.Grammar(lexer);
