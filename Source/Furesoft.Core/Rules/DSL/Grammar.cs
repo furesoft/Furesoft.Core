e5013141245b9a11838e5fcf83e443d63164ee35
furesoft <filmee24@gmail.com>
2023-05-17 10:00:02 +0200

    chore(deps): Update Packages

--- Source/Furesoft.Core/Rules/DSL/Grammar.cs
+++ Source/Furesoft.Core/Rules/DSL/Grammar.cs
@@ -1,5 +1,7 @@
 ﻿using Furesoft.PrattParser;
 using Furesoft.PrattParser.Nodes;
+using Furesoft.PrattParser.Parselets;
+using Furesoft.PrattParser.Parselets.Literals;
 
 namespace Furesoft.Core.Rules.DSL;
 
@@ -7,8 +9,28 @@ public class Grammar : Parser<AstNode>
 {
     public Grammar(Lexer tokens) : base(tokens)
     {
+        Register(PredefinedSymbols.Name, new NameParselet());
+        Register(PredefinedSymbols.Integer, new IntegerLiteralParselet());
+        Register("=", new AssignParselet());
+
+        Ternary("?", ":", (int)BindingPower.Conditional);
+        Register("(", new CallParselet());
         Group("(", ")");
-        
+
+        Prefix("+", (int)BindingPower.Prefix);
         Prefix("-", (int)BindingPower.Prefix);
+        Prefix("~", (int)BindingPower.Prefix);
+        Prefix("!", (int)BindingPower.Prefix);
+        Prefix("not", (int)BindingPower.Prefix);
+
+        Postfix("!", (int)BindingPower.PostFix);
+
+        InfixLeft("+", (int)BindingPower.Sum);
+        InfixLeft("-", (int)BindingPower.Sum);
+        InfixLeft("*", (int)BindingPower.Product);
+        InfixLeft("/", (int)BindingPower.Product);
+        InfixRight("^", (int)BindingPower.Exponent);
+        
+        InfixLeft("->", (int)BindingPower.Product);
     }
 }
--- Source/RulesTest/TestDsl.cs
+++ Source/RulesTest/TestDsl.cs
@@ -8,8 +8,11 @@ public class TestDsl
     [Fact]
     public void Not_Should_Pass()
     {
-        var lexer = new Lexer("not5");
-        lexer.AddKeyword("not");
+        var lexer = new Lexer("not 5", "test.dsl");
+        lexer.Ignore('\r');
+        lexer.Ignore(' ');
+        lexer.Ignore('\t');
+        lexer.AddSymbol("not");
 
         var parser = new Furesoft.Core.Rules.DSL.Grammar(lexer);
 
