e0561079335468f6419e1444fc67373977ed0edd
Chris <filmee24@gmail.com>
2023-05-22 18:04:13 +0200

    fix: IfParselet wrong Parslet Type

--- Source/Furesoft.Core/Rules/DSL/Parselets/IfParselet.cs
+++ Source/Furesoft.Core/Rules/DSL/Parselets/IfParselet.cs
@@ -5,19 +5,19 @@ using Furesoft.PrattParser.Parselets;
 
 namespace Furesoft.Core.Rules.DSL.Parselets;
 
-public class IfParselet : IInfixParselet<AstNode>
+public class IfParselet : IPrefixParselet<AstNode>
 {
-    public AstNode Parse(Parser<AstNode> parser, AstNode left, Token token)
+    public int GetBindingPower()
+    {
+        return (int)BindingPower.Conditional + 1;
+    }
+
+    public AstNode Parse(Parser<AstNode> parser, Token token)
     {
         var condition = parser.Parse(GetBindingPower() - 1);
         parser.Match("then");
         var body = parser.Parse(GetBindingPower() - 1);
 
         return new IfNode(condition, body);
-    }
-
-    public int GetBindingPower()
-    {
-        return (int)BindingPower.Conditional + 1;
     }
 }
