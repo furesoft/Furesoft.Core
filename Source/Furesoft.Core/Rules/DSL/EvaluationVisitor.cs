ffacf13bb660bb055f049c8b7e42965c3ae4dd29
furesoft <filmee24@gmail.com>
2023-05-25 09:03:45 +0200

    feat: Add More Operators To Visitor

--- Source/Furesoft.Core/Rules/DSL/EvaluationVisitor.cs
+++ Source/Furesoft.Core/Rules/DSL/EvaluationVisitor.cs
@@ -12,18 +12,61 @@ public class EvaluationVisitor<T> : IVisitor<AstNode, Expression>
 {
     public Expression Visit(AstNode node)
     {
+        Expression result = null;
         if (node is BinaryOperatorNode binaryOperatorNode)
         {
+            var leftVisited = Visit(binaryOperatorNode.LeftExpr);
+            var rightVisited = Visit(binaryOperatorNode.RightExpr);
+            
             switch (binaryOperatorNode.Operator.Name)
             {
                 case "+":
-                    return Expression.Add(Visit(binaryOperatorNode.LeftExpr), Visit(binaryOperatorNode.RightExpr));
+                    result = Expression.Add(leftVisited, rightVisited);
+                    break;
+                case "-":
+                    result = Expression.Subtract(leftVisited, rightVisited);
+                    break;
+                case "*":
+                    result = Expression.Multiply(leftVisited, rightVisited);
+                    break;
+                case "/":
+                    result = Expression.Divide(leftVisited, rightVisited);
+                    break;
             }
         }
+
+        if (node is PrefixOperatorNode prefixOperatorNode)
+        {
+            var visited = Visit(prefixOperatorNode.Expr);
+
+            switch (prefixOperatorNode.Operator.Name)
+            {
+                case "-":
+                    result = Expression.Negate(visited);
+                    break;
+            }
+        }
+
+        switch (node)
+        {
+            case LiteralNode<int> ci:
+                result = Expression.Constant(ci.Value);
+                return result;
+            case LiteralNode<uint> uic:
+                result = Expression.Constant(uic.Value);
+                return result;
+            case LiteralNode<string> cs:
+                result = Expression.Constant(cs.Value);
+                return result;
+            case LiteralNode<bool> cb:
+                result = Expression.Constant(cb.Value);
+                return result;
+            case LiteralNode<double> cbd:
+                result = Expression.Constant(cbd.Value);
+                return result;
+        }
         
-        
-        
-        var body = Expression.Constant(new RuleResult() { });
+        var body = Expression.Constant(new RuleResult() { Result = result });
         
         return Expression.Lambda(body, true, Expression.Parameter(typeof(T)));
     }
