b72bd32ce89391ec54540f1c96c8abcf3a24b6f4
furesoft <filmee24@gmail.com>
2023-05-25 11:35:56 +0200

    refactor: Cleanup

--- Source/Furesoft.Core/Rules/DSL/EvaluationVisitor.cs
+++ Source/Furesoft.Core/Rules/DSL/EvaluationVisitor.cs
@@ -13,11 +13,85 @@ public class EvaluationVisitor<T> : IVisitor<AstNode, Expression>
     public Expression Visit(AstNode node)
     {
         Expression result = null;
+        result = VisitBinary(node, result);
+
+        result = VisitPrefix(node, result);
+
+        if (VisitConstant(node, out var visit))
+        {
+            return visit;
+        }
+        
+        var body = Expression.Constant(new RuleResult() { Result = result });
+        
+        return Expression.Lambda(body, true, Expression.Parameter(typeof(T)));
+    }
+
+    private static bool VisitConstant(AstNode node, out Expression visit)
+    {
+        Expression result;
+        switch (node)
+        {
+            case LiteralNode<int> ci:
+                result = Expression.Constant(ci.Value);
+            {
+                visit = result;
+                return true;
+            }
+            case LiteralNode<uint> uic:
+                result = Expression.Constant(uic.Value);
+            {
+                visit = result;
+                return true;
+            }
+            case LiteralNode<string> cs:
+                result = Expression.Constant(cs.Value);
+            {
+                visit = result;
+                return true;
+            }
+            case LiteralNode<bool> cb:
+                result = Expression.Constant(cb.Value);
+            {
+                visit = result;
+                return true;
+            }
+            case LiteralNode<double> cbd:
+                result = Expression.Constant(cbd.Value);
+            {
+                visit = result;
+                return true;
+            }
+        }
+
+        visit = Expression.Empty();
+        return false;
+    }
+
+    private Expression VisitPrefix(AstNode node, Expression result)
+    {
+        if (node is PrefixOperatorNode prefixOperatorNode)
+        {
+            var visited = Visit(prefixOperatorNode.Expr);
+
+            switch (prefixOperatorNode.Operator.Name)
+            {
+                case "-":
+                    result = Expression.Negate(visited);
+                    break;
+            }
+        }
+
+        return result;
+    }
+
+    private Expression VisitBinary(AstNode node, Expression result)
+    {
         if (node is BinaryOperatorNode binaryOperatorNode)
         {
             var leftVisited = Visit(binaryOperatorNode.LeftExpr);
             var rightVisited = Visit(binaryOperatorNode.RightExpr);
-            
+
             switch (binaryOperatorNode.Operator.Name)
             {
                 case "+":
@@ -35,39 +109,6 @@ public class EvaluationVisitor<T> : IVisitor<AstNode, Expression>
             }
         }
 
-        if (node is PrefixOperatorNode prefixOperatorNode)
-        {
-            var visited = Visit(prefixOperatorNode.Expr);
-
-            switch (prefixOperatorNode.Operator.Name)
-            {
-                case "-":
-                    result = Expression.Negate(visited);
-                    break;
-            }
-        }
-
-        switch (node)
-        {
-            case LiteralNode<int> ci:
-                result = Expression.Constant(ci.Value);
-                return result;
-            case LiteralNode<uint> uic:
-                result = Expression.Constant(uic.Value);
-                return result;
-            case LiteralNode<string> cs:
-                result = Expression.Constant(cs.Value);
-                return result;
-            case LiteralNode<bool> cb:
-                result = Expression.Constant(cb.Value);
-                return result;
-            case LiteralNode<double> cbd:
-                result = Expression.Constant(cbd.Value);
-                return result;
-        }
-        
-        var body = Expression.Constant(new RuleResult() { Result = result });
-        
-        return Expression.Lambda(body, true, Expression.Parameter(typeof(T)));
+        return result;
     }
 }
